965363300af1ec6164f33b0dfd76eeba5b10ce39
Adam Coster <adam@bscotch.net>
2024-04-09 11:55:42 -0500

    feat: Added a package for handling line breaks

---
+++ packages/l10n-breaks/README.md
@@ -0,0 +1,93 @@
+# L10n Breaks
+
+There are lots of ways to determine where linebreaks in text could go, and that can vary a lot by language.
+
+This package provides helpers for various linebreak cases.
+
+Supported cases:
+
+- **Chinese & Japanese**: Breaking on ideographs while ensuring that punctuation is attached to the appropriate adjacent character, and treating non-CJ text as distinct ideographs.
+
+## Usage
+
+```typescript
+import { splitOnIdeograph } from '@bscotch/l10n-breaks';
+
+console.log(
+  splitOnIdeograph(
+    '记得长大后学习怎么12345 hello。用锤子的时候，第一课就是学会站稳脚跟。如果站不稳，那就永远都打不准。有时候往往是那些微不足道的小事，能帮你重新找到立足点。',
+  ),
+);
+```
+
+Returns:
+
+```json
+[
+  "记",
+  "得",
+  "长",
+  "大",
+  "后",
+  "学",
+  "习",
+  "怎",
+  "么",
+  "12345 hello。",
+  "用",
+  "锤",
+  "子",
+  "的",
+  "时",
+  "候，",
+  "第",
+  "一",
+  "课",
+  "就",
+  "是",
+  "学",
+  "会",
+  "站",
+  "稳",
+  "脚",
+  "跟。",
+  "如",
+  "果",
+  "站",
+  "不",
+  "稳，",
+  "那",
+  "就",
+  "永",
+  "远",
+  "都",
+  "打",
+  "不",
+  "准。",
+  "有",
+  "时",
+  "候",
+  "往",
+  "往",
+  "是",
+  "那",
+  "些",
+  "微",
+  "不",
+  "足",
+  "道",
+  "的",
+  "小",
+  "事，",
+  "能",
+  "帮",
+  "你",
+  "重",
+  "新",
+  "找",
+  "到",
+  "立",
+  "足",
+  "点。"
+]
+```
---
+++ packages/l10n-breaks/src/index.ts
@@ -0,0 +1,86 @@
+export const punctuation = {
+  noLeftBreak: [
+    '。', // Ideographic Full Stop
+    '，', // Fullwidth Comma
+    '；', // Fullwidth Semicolon
+    '：', // Fullwidth Colon
+    '？', // Fullwidth Question Mark
+    '！', // Fullwidth Exclamation Mark
+    '）', // Fullwidth Right Parenthesis
+    '】', // Right Black Lenticular Bracket
+    '」', // Right Corner Bracket
+    '』', // Right White Corner Bracket
+    '》', // Right Double Angle Bracket
+    '、', // Ideographic Comma
+    '…', // Horizontal Ellipsis
+    '—', // Em Dash
+    '-', // Hyphen-Minus
+    ',', // Comma
+    '.', // Period
+    ':', // Colon
+    ';', // Semicolon
+    '!', // Exclamation Point
+    '?', // Question Mark
+    '”', // Right Double Quotation Mark
+    '’', // Right Single Quotation Mark
+    ')', // Right Parenthesis
+    ']', // Right Square Bracket
+    '}', // Right Curly Bracket
+    '"', // Quotation Mark
+    "'", // Single Quote
+  ],
+  noRightBreak: [
+    '（', // Fullwidth Left Parenthesis
+    '【', // Left Black Lenticular Bracket
+    '「', // Left Corner Bracket
+    '『', // Left White Corner Bracket
+    '《', // Left Double Angle Bracket
+    '“', // Left Double Quotation Mark
+    '‘', // Left Single Quotation Mark
+    '(', // Left Parenthesis
+    '[', // Left Square Bracket
+    '{', // Left Curly Bracket
+    '"', // Quotation Mark
+    "'", // Single Quote
+  ],
+};
+
+/**
+ * Given a string, split it into an array of strings where
+ * each entry is a single character
+ * (if ideographic, like Chinese/Japanese) or a word unit
+ * (if English).
+ *
+ * Punctuation marks are grouped with the right or left word
+ * unit, depending on the type of punctuation.
+ *
+ * Handles mixed ideographic and English characters.
+ *
+ * (See the {@link punctuation} rules.)
+ */
+export function splitOnIdeograph(text: string) {
+  const result = [];
+  const chars = [...text];
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+    if (i === 0) {
+      result.push(char);
+      continue;
+    }
+    const priorChar = chars[i - 1];
+    if (
+      punctuation.noLeftBreak.includes(char) ||
+      punctuation.noRightBreak.includes(priorChar) ||
+      (isNotIdeographic(char) && isNotIdeographic(priorChar))
+    ) {
+      result[result.length - 1] += char;
+      continue;
+    }
+    result.push(char);
+  }
+  return result;
+}
+
+export function isNotIdeographic(glyph: string) {
+  return /^[\u0020-\u2BFF]+$/.test(glyph);
+}
